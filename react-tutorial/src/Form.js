0d15977b45628583ca9831cfdd01e958382406ef
HayatoMoromasa <bboy.hayato1tribal@gmail.com>
2021-02-12 21:04:51 +0900

    no message

--- react-tutorial/src/Form.js
+++ react-tutorial/src/Form.js
@@ -1,7 +1,25 @@
+import { useState } from 'react';// 1) 追加
+
 export const Form = () => {
+    const [ text, setText ] = useState(''); // 2) 追加
+
+    const submitForm = (e) => { // 3) 追加
+        e.preventDefault();
+        console.log(`submitForm(${text})`);
+    }
+
     return (
         <div>
-            This is Form
+            {/* 4) 変更 */}
+            <h1>Add new language</h1>
+            <form onSubmit={submitForm}>
+                <div>
+                    <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+                </div>
+                <div>
+                    <button>Add</button>
+                </div>
+            </form>
         </div>
     )
 }
