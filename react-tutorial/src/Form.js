e7b06cee6a0ad3dbff26e065f42f4b3a815cb9e9
HayatoMoromasa <bboy.hayato1tribal@gmail.com>
2021-02-14 06:52:53 +0900

    add style

--- react-tutorial/src/Form.js
+++ react-tutorial/src/Form.js
@@ -1,7 +1,31 @@
 import { useState } from 'react';
+import { Button } from './components/button'; // 1) 追加
+import styled from 'styled-components';// 2) 追加
+
+/*　3) 追加*/
+const Container = styled.div`
+    padding: 12px 64px;
+`
+const Label = styled.label`
+    display: flex;
+    color: #757575;
+    font-size: 14px;
+    font-weight: bold;
+`
+const Input = styled.input`
+   border-radius: 3px;
+   padding; 4px 8px;
+   border: 1px solid black;
+`
+const ButtonContainer = styled.div`
+    margin-top: 24px;
+`
+const FormButton = styled(Button)`/* Buttonのスタイルを継承できる*/
+    width: 120px;
+`
+
 
 export const Form = ({ onAddLang }) => {
-    console.warn('Form.js');// 1) 追加
     const [ text, setText ] = useState('');
 
     const submitForm = (e) => {
@@ -10,16 +34,18 @@ export const Form = ({ onAddLang }) => {
     }
 
     return (
-        <div>
+        /* 4) それぞれタグ名変更*/
+        <Container>
             <h1>Add new language</h1>
             <form onSubmit={submitForm}>
                 <div>
-                    <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+                    <Label>Language</Label>
+                    <Input type="text" value={text} onChange={(e) => setText(e.target.value)} />
                 </div>
-                <div>
-                    <button>Add</button>
-                </div>
+                <ButtonContainer>
+                    <FormButton>Add</FormButton>
+                </ButtonContainer>
             </form>
-        </div>
+        </Container>
     )
 }
--- react-tutorial/src/List.js
+++ react-tutorial/src/List.js
@@ -1,11 +1,24 @@
+import styled from 'styled-components';// 1) 追加
+
+// 2) 追加
+const Container = styled.div`
+    padding: 8px 16px;
+`
+const ListItem = styled.div`
+    padding: 8px 16px;
+    &:nth-child(n+2) {
+        border-top: 1px solid #D9DBDE;
+    }
+`
+
 export const List = ({ langs }) => {
     return (
-        <div>
+        <Container>{/* 3) 追加 */}
             {
                 langs.map((lang, index) => {
-                    return <div key={ index }>{ lang }</div>
+                    return <ListItem key={ index }>{ lang }</ListItem>　/*　4) 追加*/
                 })
             }
-        </div>
+        </Container>
     )
 }
